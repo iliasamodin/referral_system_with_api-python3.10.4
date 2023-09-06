b3763e8d3082bce9d6c2640dc7fe297ad9615ae2
iliasamodin <broobooot@gmail.com>
2023-09-06 14:39:36 +0500

    create a model with expected keys to API routes

--- instruction/models.py
+++ instruction/models.py
@@ -9,3 +9,35 @@ class APIPath(models.Model):
 
     def __str__(self):
         return self.path
+
+
+# Model of accepted keys for api addresses
+class Key(models.Model):
+    GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS, TRACE = \
+        "get", "post", "put", "patch", "delete", "head", "options", "trace"
+    REQUEST_OPTIONS = [
+        (GET, "get"),
+        (POST, "post"),
+        (PUT, "put"),
+        (PATCH, "patch"),
+        (DELETE, "delete"),
+        (HEAD, "head"),
+        (OPTIONS, "options"),
+        (TRACE, "trace")
+    ]
+
+    api_path = models.ForeignKey(APIPath, null=False, on_delete=models.CASCADE)
+    key = models.CharField(max_length=50, default="", blank=True)
+    request_type = models.CharField(
+        max_length=7,
+        choices=REQUEST_OPTIONS,
+        default=GET
+    )
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["api_path", "id"]
+
+    def __str__(self):
+        return f"{self.key} for {self.api_path.path}"
